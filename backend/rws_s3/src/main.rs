7cc42ac6a11b3c3d28b81c070c168a7206e8fc2a
Hillel Nagid <hillel123123@gmail.com>
2024-01-15 16:28:23 +0200

    added postgresql

--- backend/rws_s3/src/main.rs
+++ backend/rws_s3/src/main.rs
@@ -7,13 +7,23 @@ use filesystem::{
     bucket::create_bucket,
     object::{create_object, get_object},
 };
+use tokio_postgres::{Error, NoTls};
 
 #[tokio::main]
 async fn main() {
+    let (client, connection) = tokio_postgres::connect("host=localhost user=postgres", NoTls)
+        .await
+        .unwrap();
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
     let app = Router::new()
         .route("/createBucket/:bucketid", put(create_bucket))
         .route("/:bucketid/*objectpath", put(create_object).get(get_object))
-        .layer(DefaultBodyLimit::max(204800)); //limits to 200MB file upload
+        .layer(DefaultBodyLimit::max(204800)) //limits to 200MB file upload
+        .with_state(&client); //Fix lifetime issue
     let listener = tokio::net::TcpListener::bind("0.0.0.0:2945").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
