02a17010ce3fa6646cad8b9c8ec1c5240a66a300
Hillel Nagid <hillel123123@gmail.com>
2024-01-16 15:38:26 +0200

    postgres works!!!

--- backend/rws_s3/src/filesystem/object.rs
+++ backend/rws_s3/src/filesystem/object.rs
@@ -1,117 +1,102 @@
 use axum::{
-    extract::{Multipart, Path},
+    extract::{Multipart, Path, State},
     http::{header::CONTENT_TYPE, StatusCode},
     response::{AppendHeaders, IntoResponse, Response},
     Json,
 };
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
 use serde_json::{json, Value};
 use std::{
     env::set_current_dir,
     fs::{create_dir_all, read, File},
-    io::Write,
+    io::{Error, ErrorKind, Write},
 };
+use tokio_postgres::NoTls;
 
+use crate::internal_error;
+
+type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 pub async fn create_object(
+    State(pool): State<ConnectionPool>,
     Path((bucketid, path)): Path<(String, String)>,
     mut multipart: Multipart,
-) -> (StatusCode, Json<Value>) {
+) -> Result<Json<Value>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
     if set_current_dir(format!("storage/{}", bucketid)).is_ok() {
         let mut path_vec: Vec<&str> = path.split("/").collect();
         if let Some(object_name) = path_vec.pop() {
             if create_dir_all(path_vec.join("/")).is_ok() {
                 if set_current_dir(path_vec.join("/")).is_ok() {
                     let file = File::create(object_name);
-                    match file {
+                    match file.map_err(internal_error) {
                         Ok(mut f) => {
                             while let Some(field) = multipart.next_field().await.unwrap() {
                                 let data = field.bytes().await.unwrap();
                                 if f.write(&data).is_err() {
-                                    return (
+                                    return Err((
                                         StatusCode::INTERNAL_SERVER_ERROR,
-                                        Json(json!({
-                                          "result":
-                                          "Failed writing to provided file"
-                                        })),
-                                    );
+                                        "Failed writing to provided file".to_owned(),
+                                    ));
                                 }
                             }
                         }
-                        Err(e) => {
-                            return (
-                                StatusCode::INTERNAL_SERVER_ERROR,
-                                Json(json!({
-                                    "result":
-                                        format!("Failed creating provided file with error: {:?}", e)
-                                })),
-                            )
-                        }
+                        Err(e) => return Err(e),
                     };
                 } else {
-                    return (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!({"result":"Failed setting working dir to provided path"})),
-                    );
+                    return Err(internal_error(Error::new(
+                        ErrorKind::Other,
+                        "Failed setting working dir to provided path",
+                    )));
                 }
             } else {
-                return (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    Json(json!({"result":"Failed creating provided path"})),
-                );
+                return Err(internal_error(Error::new(
+                    ErrorKind::Other,
+                    "Could't find provided path",
+                )));
             }
         };
-        return (StatusCode::CREATED, Json(json!({"result":"Success"})));
+        return Ok(Json(json!({"result":"Success"})));
     } else {
-        return (
-            StatusCode::BAD_REQUEST,
-            Json(json!({"result":"Couln't find bucket"})),
-        );
+        return Err((StatusCode::BAD_REQUEST, "Couln't find bucket".to_owned()));
     }
 }
 
-pub async fn get_object(Path((bucketid, path)): Path<(String, String)>) -> Response {
+pub async fn get_object(
+    Path((bucketid, path)): Path<(String, String)>,
+) -> Result<Response, (StatusCode, String)> {
     if set_current_dir(format!("storage/{}", bucketid)).is_ok() {
         let mut path_vec: Vec<&str> = path.split("/").collect();
         if let Some(object_name) = path_vec.pop() {
             if set_current_dir(path_vec.join("/")).is_ok() {
-                match read(object_name) {
+                match read(object_name).map_err(internal_error) {
                     Ok(file) => {
-                        return (
+                        return Ok((
                             StatusCode::OK,
                             AppendHeaders([(CONTENT_TYPE, "text/plain")]),
                             file,
                         )
-                            .into_response();
+                            .into_response());
                     }
 
-                    Err(err) => {
-                        return (
-                            StatusCode::BAD_REQUEST,
-                            Json(json!({
-                                "err": format!("Couln't find file. error: {:?}", err)
-                            })),
-                        )
-                            .into_response();
-                    }
+                    Err(err) => return Err(err),
                 }
             } else {
-                return (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    Json(json!({"err":"Could't set work dir as file path"})),
-                )
-                    .into_response();
+                return Err(internal_error(Error::new(
+                    ErrorKind::Other,
+                    "Could't set work dir as file path",
+                )));
             }
         } else {
-            return (
-                StatusCode::BAD_REQUEST,
-                Json(json!({"err":"Could't get file name"})),
-            )
-                .into_response();
+            return Err(internal_error(Error::new(
+                ErrorKind::Other,
+                "Could't get file name",
+            )));
         }
     } else {
-        return (
-            StatusCode::BAD_REQUEST,
-            Json(json!({"err":"Couln't find bucket"})),
-        )
-            .into_response();
+        return Err(internal_error(Error::new(
+            ErrorKind::Other,
+            "Could't find bucket",
+        )));
     }
 }
--- backend/rws_s3/src/main.rs
+++ backend/rws_s3/src/main.rs
@@ -1,29 +1,35 @@
-use axum::{extract::DefaultBodyLimit, routing::put, Router};
+use std::sync::Arc;
+
+use axum::{extract::DefaultBodyLimit, http::StatusCode, routing::put, Router};
 mod filesystem {
     pub mod bucket;
     pub mod object;
 }
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
 use filesystem::{
     bucket::create_bucket,
     object::{create_object, get_object},
 };
-use tokio_postgres::{Error, NoTls};
-
+use tokio_postgres::NoTls;
 #[tokio::main]
 async fn main() {
-    let (client, connection) = tokio_postgres::connect("host=localhost user=postgres", NoTls)
-        .await
-        .unwrap();
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+    let manager =
+        PostgresConnectionManager::new_from_stringlike("host=localhost user=postgres", NoTls)
+            .unwrap();
+    let pool = Pool::builder().build(manager).await.unwrap();
+
     let app = Router::new()
         .route("/createBucket/:bucketid", put(create_bucket))
         .route("/:bucketid/*objectpath", put(create_object).get(get_object))
         .layer(DefaultBodyLimit::max(204800)) //limits to 200MB file upload
-        .with_state(&client); //Fix lifetime issue
+        .with_state(pool);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:2945").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
