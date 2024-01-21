7cb82493ae62798258a4cb6dae0248f389023a1b
Hillel Nagid <hillel123123@gmail.com>
2024-01-21 16:02:21 +0200

    added postgres integration

--- backend/rws_s3/src/filesystem/object.rs
+++ backend/rws_s3/src/filesystem/object.rs
@@ -1,20 +1,25 @@
+use crate::internal_error;
 use axum::{
     extract::{Multipart, Path, State},
-    http::{header::CONTENT_TYPE, StatusCode},
+    http::{
+        header::{CONTENT_DISPOSITION, CONTENT_TYPE},
+        StatusCode,
+    },
     response::{AppendHeaders, IntoResponse, Response},
     Json,
 };
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
+use chrono::prelude::*;
+use md5;
 use serde_json::{json, Value};
 use std::{
     env::set_current_dir,
-    fs::{create_dir_all, read, File},
+    fs::{self, create_dir_all, read, File},
     io::{Error, ErrorKind, Write},
 };
-use tokio_postgres::NoTls;
-
-use crate::internal_error;
+use tokio_postgres::{types::Type, NoTls};
+use uuid::Uuid;
 
 type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 pub async fn create_object(
@@ -23,7 +28,7 @@ pub async fn create_object(
     mut multipart: Multipart,
 ) -> Result<Json<Value>, (StatusCode, String)> {
     let conn = pool.get().await.map_err(internal_error)?;
-    if set_current_dir(format!("storage/{}", bucketid)).is_ok() {
+    if set_current_dir(format!("/storage/{}", bucketid)).is_ok() {
         let mut path_vec: Vec<&str> = path.split("/").collect();
         if let Some(object_name) = path_vec.pop() {
             if create_dir_all(path_vec.join("/")).is_ok() {
@@ -31,15 +36,69 @@ pub async fn create_object(
                     let file = File::create(object_name);
                     match file.map_err(internal_error) {
                         Ok(mut f) => {
+                            let mut content_disposition = String::from("inline");
+                            let mut content_length = 0u32;
+                            let mut content_type = String::from("text/plain");
+                            let mut etag = String::from("");
+                            let mut encrypted: bool = false;
                             while let Some(field) = multipart.next_field().await.unwrap() {
-                                let data = field.bytes().await.unwrap();
-                                if f.write(&data).is_err() {
-                                    return Err((
-                                        StatusCode::INTERNAL_SERVER_ERROR,
-                                        "Failed writing to provided file".to_owned(),
-                                    ));
+                                if let Some(name) = &field.name() {
+                                    match name {
+                                        &"content-disposition" => {
+                                            content_disposition =
+                                                field.text().await.map_err(internal_error)?
+                                        }
+                                        &"content-type" => {
+                                            content_type =
+                                                field.text().await.map_err(internal_error)?
+                                        }
+                                        &"encrypted" => {
+                                            encrypted = match field
+                                                .text()
+                                                .await
+                                                .map_err(internal_error)?
+                                                .parse::<bool>()
+                                            {
+                                                Ok(value) => value,
+                                                Err(_) => false,
+                                            }
+                                        }
+                                        &"file" => {
+                                            let data = field.bytes().await.unwrap();
+                                            content_length = data.len() as u32;
+                                            etag = format!("{:x}", md5::compute(&data));
+                                            if f.write(&data).is_err() {
+                                                return Err((
+                                                    StatusCode::INTERNAL_SERVER_ERROR,
+                                                    "Failed writing to provided file".to_owned(),
+                                                ));
+                                            }
+                                        }
+                                        _ => {}
+                                    }
                                 }
                             }
+                            println!("parsed multipart");
+                            let statement = conn.prepare("INSERT INTO \"objects\" (uid,name,upload_date,content_disposition,content_length,content_type,last_modified,etag,encrypted) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)").await.map_err(internal_error)?;
+                            let id = Uuid::new_v4();
+                            let upload_date = Utc::now().timestamp();
+                            conn.execute(
+                                &statement,
+                                &[
+                                    &id,
+                                    &path,
+                                    &upload_date,
+                                    &content_disposition,
+                                    &content_length,
+                                    &content_type,
+                                    &upload_date,
+                                    &etag,
+                                    &encrypted,
+                                ],
+                            )
+                            .await
+                            .map_err(internal_error)?;
+                            println!("executed");
                         }
                         Err(e) => return Err(e),
                     };
@@ -58,22 +117,42 @@ pub async fn create_object(
         };
         return Ok(Json(json!({"result":"Success"})));
     } else {
+        let paths = fs::read_dir("./").unwrap();
+
+        for path in paths {
+            println!("Name: {}", path.unwrap().path().display())
+        }
         return Err((StatusCode::BAD_REQUEST, "Couln't find bucket".to_owned()));
     }
 }
 
 pub async fn get_object(
+    State(pool): State<ConnectionPool>,
     Path((bucketid, path)): Path<(String, String)>,
 ) -> Result<Response, (StatusCode, String)> {
-    if set_current_dir(format!("storage/{}", bucketid)).is_ok() {
+    let conn = pool.get().await.map_err(internal_error)?;
+    if set_current_dir(format!("/storage/{}", bucketid)).is_ok() {
         let mut path_vec: Vec<&str> = path.split("/").collect();
         if let Some(object_name) = path_vec.pop() {
             if set_current_dir(path_vec.join("/")).is_ok() {
                 match read(object_name).map_err(internal_error) {
                     Ok(file) => {
+                        let statement = conn.prepare(
+                            "SELECT (content_type, content_disposition) FROM objects WHERE name=$1",
+                        ).await.map_err(internal_error)?;
+                        let row = conn
+                            .query_one(&statement, &[&object_name])
+                            .await
+                            .map_err(internal_error)?;
+                        let row_record: Type = row.get("row");
+                        let content_type: String = row_record.get("content_type");
+                        let content_disposition: String = row_record.get("content_disposition");
                         return Ok((
                             StatusCode::OK,
-                            AppendHeaders([(CONTENT_TYPE, "text/plain")]),
+                            AppendHeaders([
+                                (CONTENT_TYPE, content_type),
+                                (CONTENT_DISPOSITION, content_disposition),
+                            ]),
                             file,
                         )
                             .into_response());
